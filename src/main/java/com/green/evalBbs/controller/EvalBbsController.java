66099d42fc59bb22da71b6ebc982f8265443b4e1
kim <kyungmin3946@naver.com>
2024-04-26 15:08:41 +0900

    개발환경 및 기본페이지 세팅

---
+++ src/main/java/com/green/evalBbs/controller/EvalBbsController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.green.evalBbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EvalBbsController {
+
+	@RequestMapping("/")
+	public@ResponseBody  String index() {
+		
+		return "check";
+	}
+	
+	
+}
---
+++ src/main/java/com/green/evalBbs/dao/IEvalBbsDao.java
@@ -0,0 +1,5 @@
+package com.green.evalBbs.dao;
+
+public interface IEvalBbsDao {
+
+}
